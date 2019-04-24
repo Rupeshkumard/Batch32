fe26810307c10b3de9be02b56db614d943bcfaef
Rupeshkumard <rupesh.talekar81296@gmail.com>
2019-04-24 15:58:45 +0530

    first commit

---
+++ src/Bluestone1.java
@@ -0,0 +1,20 @@
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Bluestone1 {
+	static {
+		System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+	}
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.bluestone.com");
+		driver.findElement(By.name("search_query")).sendKeys("rings");
+		driver.findElement(By.xpath("//input[contains(@name,'search') and @class='button']"));
+		
+	}
+
+}
